80fc5524139075491fb02e250124bfb0dd2a7b3f
ydsong2 <ydsong2@gmail.com>
2023-01-22 12:30:49 +0000

    Feat: add predictor module, modify face_embedding module

--- face_embedding/main.py
+++ face_embedding/main.py
@@ -1,5 +1,7 @@
 import os
+import math
 import cv2
+import json
 import pandas as pd
 import numpy as np
 import onnxruntime
@@ -7,8 +9,8 @@ from scrfd import SCRFD
 from arcface_onnx import ArcFaceONNX
 
 
-def detect_face(image: str):
-    image = cv2.imread(image)
+def detect_face(image: np.ndarray) -> np.ndarray:
+    # image = cv2.imread(image)
     bboxes, _ = detector.autodetect(image, max_num=1)
     if bboxes.shape[0] == 0:
         return -1.0, "Face not found in Image"
@@ -16,8 +18,36 @@ def detect_face(image: str):
         return bboxes
 
 
-def compute_face_feature(image: str) -> np.ndarray:
-    image = cv2.imread(image)
+def simple_softmax(confidence: dict) -> dict:
+    confidence = {key: pow(math.e, value) for key, value in confidence.items()}
+    denominator = sum(confidence.values())
+    confidence = {
+        key: round(value / denominator, 4) for key, value in confidence.items()
+    }
+    return confidence
+
+
+def compute_face_feature(row) -> np.ndarray:
+
+    image = cv2.imread(row["filename"])
+
+    ## normalized scaling
+    # H, W, _ = image.shape
+    # xmin = int(abs(row["track_body_xmin"]) * W)
+    # xmax = int(abs(row["track_body_xmax"]) * W)
+    # ymin = int(abs(row["track_body_ymin"]) * H)
+    # ymax = int(abs(row["track_body_ymax"]) * H)
+
+    xmin = int(row["track_body_xmin"])
+    xmax = int(row["track_body_xmax"])
+    ymin = int(row["track_body_ymin"])
+    ymax = int(row["track_body_ymax"])
+
+    image = image[
+        ymin:ymax,
+        xmin:xmax,
+    ]
+
     bboxes, kpss = detector.autodetect(image, max_num=1)
     if bboxes.shape[0] == 0:
         return np.zeros(512)
@@ -38,6 +68,7 @@ def compute_face_confidence(
         key: round(rec.compute_sim(np.array(value), face_feature), 4)
         for key, value in anchor_face_embedding.items()
     }
+
     return face_confidence
 
 
@@ -45,15 +76,60 @@ def face_embedding_extractor(
     df1: pd.DataFrame, df2: pd.DataFrame, anchor_face_embedding: dict
 ) -> pd.DataFrame:
 
-    df2["face_embedding"] = (
-        df1.loc[df2["df1_index"]]["filename"]
-        .map(lambda x: os.path.join("/opt/ml/data/frame_1080p", x))
-        .map(compute_face_feature)
-        .values
+    df2[
+        [
+            "filename",
+            "track_body_xmin",
+            "track_body_ymin",
+            "track_body_xmax",
+            "track_body_ymax",
+        ]
+    ] = df1.loc[df2["df1_index"]][
+        [
+            "filename",
+            "track_body_xmin",
+            "track_body_ymin",
+            "track_body_xmax",
+            "track_body_ymax",
+        ]
+    ].reset_index(
+        drop=True
     )
-    df2["face_confidence"] = df2["face_embedding"].map(
-        lambda x: compute_face_confidence(x, anchor_face_embedding)
+
+    # df2["filename"] = df2["filename"].map(
+    #     lambda x: os.path.join("/opt/ml/data/frame_1080p", x)
+    # )
+    df2["filename"] = df2["filename"].map(
+        lambda x: os.path.join(meta_json["image_root"], x)
     )
+
+    df2["face_embedding"] = df2.apply(compute_face_feature, axis=1)
+
+    df2.drop(
+        [
+            "filename",
+            "track_body_xmin",
+            "track_body_ymin",
+            "track_body_xmax",
+            "track_body_ymax",
+        ],
+        axis=1,
+        inplace=True,
+    )
+
+    # df2["face_embedding"] = (
+    #     df1.loc[df2["df1_index"]]["filename"]
+    #     .map(lambda x: os.path.join("/opt/ml/data/frame_1080p", x))
+    #     .map(compute_face_feature)
+    #     .reset_index(drop=True)
+    # )
+
+    df2["face_confidence"] = (
+        df2["face_embedding"]
+        .map(lambda x: compute_face_confidence(x, anchor_face_embedding))
+        .map(simple_softmax)
+    )
+
     df2["face_pred"] = df2["face_confidence"].map(lambda x: max(x, key=x.get))
 
     # df2.to_csv("/opt/ml/torchkpop/df2.csv", sep=",")
@@ -62,9 +138,18 @@ def face_embedding_extractor(
 
 # if __name__ == "__main__":
 
+for meta_json in os.listdir("./data/"):
+    if os.path.splitext(meta_json)[-1] == ".json":
+        break
+
+with open(os.path.join("./data", meta_json), "r", encoding="utf-8") as f:
+    meta_json = json.load(f)
+
+
 onnxruntime.set_default_logger_severity(3)
 
-root_dir = "/opt/ml/torchkpop/face_embedding/"
+# root_dir = "/opt/ml/torchkpop/face_embedding/"
+root_dir = "./face_embedding"
 
 detector = SCRFD(os.path.join(root_dir, "model/det_10g.onnx"))
 detector.prepare(0)
