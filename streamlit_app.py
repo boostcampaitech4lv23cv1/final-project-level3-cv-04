06e959aeeaa2d74ac89588ff920c6b6638e8cf7d
brotherhoon <brotherhoon88@gmail.com>
2023-01-26 09:25:06 +0000

    Feat: add streamlit entry_page

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,26 +3,34 @@ import time
 import numpy as np
 import re
 import streamlit as st
-# from streamlit_terran_timeline import generate_timeline, terran_timeline
 
-def start_entry_page():
+st.set_page_config(initial_sidebar_state="collapsed")
+
+
+
+def entry_page():
     st.title("Torch-kpop")
     st.title("AI makes personal videos for you 😍")
     url = st.text_input(label="Input youtube URL 🔻", placeholder="https://www.youtube.com/watch?v=0lXwMdnpoFQ")
-    
-    # if click ok btn
-    if st.button("ok"):
+        
+    if st.button("INPUT"):
+
         # check youtube regex reference from https://stackoverflow.com/questions/19377262/regex-for-youtube-url
         if re.match("^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$", url):
-            # if matching youtube url, show video
+            
+            # if matching youtube url, show input video
             video = st.video(url)
-            st.button("make video")
+
+            # show button moe_running_btnng
+            if st.button("MAKE VIDEO"):
+                pass
             
         else:
             st.write("This is not a valid youtube url")
 
+entry_page()
+
+
     
 
 
-if __name__ == "__main__":
-    start_entry_page()
