a65edd160905371d20f8618e5eb762e09305c2f8
astron8t <dykim2015@snu.ac.kr>
2023-01-28 20:49:26 +0000

    Feat: divide-ID-by-IoU feature

--- mmtracking/utils/Postprocesser.py
+++ mmtracking/utils/Postprocesser.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 import mmtracking.utils.libs.iou as iou
 import json
-
-
+from tqdm import tqdm
+from itertools import combinations
 
 # default just keeping ids which over 5sec 
 def delete_short_id(df:pd.DataFrame, fps:int, sec:int=5)-> pd.DataFrame:
@@ -85,7 +85,72 @@ def count_overlap(df:pd.DataFrame)->pd.DataFrame:
     df["isfront"] = isfront_array
     return df
 
-def postprocessing(df:pd.DataFrame, meta_info:dict, sec:int=5):
+
+def compute_IOU(bbox1: list, 
+                bbox2: list):
+    bbox1_area = (bbox1[2]-bbox1[0]) * (bbox1[3]-bbox1[1])
+    bbox2_area = (bbox2[2]-bbox2[0]) * (bbox2[3]-bbox2[1])
+    
+    # obtain x1, y1, x2, y2 of the intersection
+    xlen = max(0, min(bbox1[2], bbox2[2]) - max(bbox1[0], bbox2[0]))
+    ylen = max(0, min(bbox1[3], bbox2[3]) - max(bbox1[1], bbox2[1]))
+    
+    overlapped = xlen * ylen
+    
+    return overlapped / (bbox1_area + bbox2_area - overlapped)
+
+
+def assign_new_id(starting_frame: int,
+                  df: pd.DataFrame,
+                  track_id: int,
+                  new_track_id: int,
+                  verbose = False):       # starting_frame (포함)이후의 df에서, track_id 들을 new_track_id 로 바꾼 df를 반환함
+    if verbose:
+        print('originally, id {} count: {}'.format(track_id, df[df['track_id'] == track_id]['track_id'].count()))
+    df.loc[(df['frame'] >= starting_frame) & (df['track_id'] == track_id), 'track_id'] = new_track_id
+    if verbose:
+        print('newly assinged id {} by count: {}'.format(new_track_id, df[df['track_id'] == new_track_id]['track_id'].count()))
+    
+    return df
+
+
+def divide_ids_by_IOU(df, IOU_THRESHOLD = 0.6):
+    maxframenum = df['frame'].values[-1]
+    next_track_id = max(df['track_id'].values) + 1
+    to_divide_ids = set()
+
+    for i in tqdm(range(1, maxframenum+1)):
+        now_df = df[df['frame']==i].reset_index(drop=True)
+        now_overlapped_ids = set()
+        for a, b in combinations(range(len(now_df)), 2):    #현재 프레임의 row 에서 두 개를 뽑은 조합을 모두 IoU 계산
+            bbox1 = now_df.loc[a][['track_body_xmin', 'track_body_ymin', 'track_body_xmax', 'track_body_ymax']].values
+            bbox2 = now_df.loc[b][['track_body_xmin', 'track_body_ymin', 'track_body_xmax', 'track_body_ymax']].values
+            if compute_IOU(bbox1, bbox2) > IOU_THRESHOLD:
+                overlapped_1 = now_df.loc[a]['track_id']
+                overlapped_2 = now_df.loc[b]['track_id']
+                now_overlapped_ids.update([overlapped_1, overlapped_2])
+        
+        # 원래는 겹치고 있다가 이번 프레임부터 겹치지 않은 id 들에 대해, 이후 프레임들에서 새 id를 부여해서 분리
+        to_remove = set()
+        for d_id in to_divide_ids:
+            if d_id not in now_overlapped_ids:
+                df = assign_new_id(i, df, d_id, next_track_id)
+                next_track_id += 1
+                to_remove.add(d_id)
+        for del_id in to_remove:
+            to_divide_ids.remove(del_id)
+        
+        for d_id in now_overlapped_ids:
+            if d_id not in to_divide_ids:
+                to_divide_ids.add(d_id)
+    
+    return df
+
+
+def postprocessing(df:pd.DataFrame, meta_info:dict, sec:int=1):
+    # divide ids by IOU
+    df = divide_ids_by_IOU(df)
+    
     # del short length ids, threshold 3sec
     df = delete_short_id(df, meta_info['fps'], sec)
 
@@ -95,6 +160,7 @@ def postprocessing(df:pd.DataFrame, meta_info:dict, sec:int=5):
     # clip the dataframe
     df = clip(df, meta_info)
     return df
+
 
 if __name__ == "__main__":
     RAW_DF1_PATH = "/opt/ml/final-project-level3-cv-04/test_threshold_05/df1_raw.csv"
